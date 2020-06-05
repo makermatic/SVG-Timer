6b3be7631485b7cdfb6e0bfcdfecebeba765a912
makermatic <makermatic@users.noreply.github.com>
2020-06-05 11:30:25 -0400

    removed node js stuff

--- sketch.js
+++ sketch.js
@@ -10,16 +10,11 @@ function preload() {
 
 function setup() {
   createCanvas(600, 600)
-<<<<<<< HEAD:public/sketch.js
   textFont(tFont)
   timer = new Timer(seconds, 500, "white")
 
-  socket = socket.io.connect("https://svgtimer.netlify.app/")
-  socket.on('keyCode', newDrawing)
-=======
   textFont(timerFont)
   timer = new Timer(30, 500, "white")
->>>>>>> parent of b3424fd... Added sec and fps vars, made stop() method:sketch.js
 }
 
 function draw() {
@@ -27,23 +22,23 @@ function draw() {
     thing: keyCode,
   }
 
-  socket.emit (keyCode)
-  
+  socket.emit(keyCode)
+
   clear() //<-- transparent bg
 
   //background/square setup 
-  fill ("#9400FF")
-  stroke ("white")
+  fill("#9400FF")
+  stroke("white")
   strokeWeight(3)
-  rect (0, 0, width, height)
+  rect(0, 0, width, height)
 
 
   timer.display()
-  
+
   //C key toggles timer
   if (keyTyped && keyCode === 67) {
     timer.go()
-  //Any other key stops timer
+    //Any other key stops timer
   } else if (keyTyped) {
     timer.stop()
   }
